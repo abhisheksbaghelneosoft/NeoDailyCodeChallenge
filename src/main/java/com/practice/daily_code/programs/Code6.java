de75fde04e1c69bb8e28a100f47c2a183d5af115
abhisheksbaghelneosoft <abhishek.baghel@neosoftmail.com>
2023-07-31 12:21:54 +0530

    array repetition in order

---
+++ src/main/java/com/practice/daily_code/programs/Code6.java
@@ -0,0 +1,42 @@
+package com.practice.daily_code.programs;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class Code6 {
+
+    public static void main(String[] args) {
+        int input[]= {5, 4, 5, 5, 3, 1, 2, 2, 4};
+        int output[]= new int[input.length];
+//        expected output = { 1, 1, 1, 2, 2, 3 }
+//        expected output = {5, 5, 5, 4, 4, 3, 1, 2, 2 }
+        HashSet<Integer> hSet= new LinkedHashSet<>();
+        for(int x: input)
+        {
+            hSet.add(x);
+        }
+        int temp=0;
+        int j=0;
+        Iterator it= hSet.iterator();
+        while (it.hasNext())
+        {
+            temp= (int) it.next();
+            for (int i = 0; i < input.length; i++) {
+                if(temp==(input[i]))
+                {
+                    output[j]=input[i];
+                    j++;
+                }
+            }
+        }
+
+        for(int x : output)
+        {
+            System.out.print(x+" ");
+        }
+
+
+    }
+
+}
