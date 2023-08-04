78c29e70e84122600371225522bc8f20227489a5
abhisheksbaghelneosoft <abhishek.baghel@neosoftmail.com>
2023-08-04 20:09:30 +0530

    improved commit

--- src/main/java/com/practice/daily_code/programs/Code11.java
+++ src/main/java/com/practice/daily_code/programs/Code11.java
@@ -1,5 +1,8 @@
 package com.practice.daily_code.programs;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Code11 {
 
     public static void main(String[] args) {
@@ -7,8 +10,9 @@ public class Code11 {
         int arr[] = { 3, 1, 2, 6, 9, 4 };
         int k=5;
 
+        Set<Integer> hSet= new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 if(((arr[i]+arr[j])%5)==0)
                 {
                     System.out.println("("+arr[i]+","+arr[j]+")");
