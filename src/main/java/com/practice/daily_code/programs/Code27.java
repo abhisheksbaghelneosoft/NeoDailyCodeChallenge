1f6b08073cf5bcf21fe318a51fe1dcfbef7d5465
abhisheksbaghelneosoft <abhishek.baghel@neosoftmail.com>
2023-08-21 12:10:08 +0530

    dailycommit

---
+++ src/main/java/com/practice/daily_code/programs/Code27.java
@@ -0,0 +1,48 @@
+package com.practice.daily_code.programs;
+
+public class Code27 {
+
+    public static void main(String[] args) {
+
+        int n= 0;
+        int m=5;
+        int min= 0;
+        int max= 0;
+        int temp=0;
+
+        if(n==0)
+        {
+            System.out.println("Alert!! Division by Zero");
+        }
+
+        else if(n<0)
+        {
+            for (int i = -1; i > n-m; i--) {
+                if(Math.abs(i)%m==0)
+                {
+                    if(min>i)
+                    {
+                        temp=min;
+                        min=((Math.abs(i)-Math.abs(n))>(Math.abs(n)-Math.abs(min)))?temp:i;
+                    }
+                }
+            }
+            System.out.println(min);
+        }
+
+        else {
+            for (int i = 1; i < n+m; i++) {
+                if(i%m==0)
+                {
+                    if(max<i)
+                    {
+                        temp=max;
+                        max=(n-max>i-n)?i:temp;
+                    }
+                }
+            }
+            System.out.println(max);
+        }
+    }
+}
+
